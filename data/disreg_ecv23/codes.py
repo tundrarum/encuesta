946e57f6d69f90b3d197ba69e26be336b9634758
Jaime Melis <jmelis@redhat.com>
2024-06-19 11:34:54 +0100

    formatting

--- data/disreg_ecv23/codes.py
+++ data/disreg_ecv23/codes.py
@@ -16,10 +16,10 @@ os.mkdir(CODE_DIR)
 maps = defaultdict(list)
 code_map = {}
 
-for file in glob('*.xlsx'):
+for file in glob("*.xlsx"):
     wb = openpyxl.load_workbook(file)
     for sheet in wb.worksheets:
-        if sheet.title == 'Diseño':
+        if sheet.title == "Diseño":
             rows = sheet.iter_rows(values_only=True)
 
             # skip two first rows
@@ -30,13 +30,12 @@ for file in glob('*.xlsx'):
             for row in rows:
                 f1, f2 = row[:2]
 
-                if f1 == '*** TOTAL ***':
+                if f1 == "*** TOTAL ***":
                     break
 
                 if f1 and f2:
                     code_map[f1] = f2
-
-        elif sheet.title.startswith('Tablas'):
+        elif sheet.title.startswith("Tablas"):
             map = None
             in_key = None
             rows = sheet.iter_rows(values_only=True)
@@ -48,16 +47,15 @@ for file in glob('*.xlsx'):
                     next(rows)
                 elif in_key:
                     if f1 is not None:
-                        maps[map].append((f1,f2))
+                        maps[map].append((f1, f2))
                     else:
                         in_key = False
 
 # Write CSVs
 for code, map in code_map.items():
     print([code, map])
-    with open(f'{CODE_DIR}/{code}.csv', 'w') as codefile:
+    with open(f"{CODE_DIR}/{code}.csv", "w") as codefile:
         code_writer = csv.writer(codefile)
         code_writer.writerow(["id", "description"])
         for key, val in maps[map]:
-            print([key, val])
             code_writer.writerow([key, val])
