48d8241ae4db8fa53faaaf2a1013020cee2929d7
Jaime Melis <jmelis@redhat.com>
2024-06-20 17:53:03 +0100

    change strategy to build maps instead of codes
    less tables (but uses views)

--- ecv.py
+++ ecv.py
@@ -15,11 +15,12 @@ DB_NAME = "db/ecv.duckdb"
 shutil.rmtree(CODE_DIR, ignore_errors=True)
 os.mkdir(CODE_DIR)
 
-for file in glob("data/disreg_ecv23/*.xlsx"):
-    maps = defaultdict(list)
-    code_map = {}
+maps = defaultdict(list)
+code_map = {}
 
+for file in glob("data/disreg_ecv23/*.xlsx"):
     wb = openpyxl.load_workbook(file)
+    wb_name = os.path.splitext(os.path.basename(file))[0]
     for sheet in wb.worksheets:
         if sheet.title == "Diseño":
             rows = sheet.iter_rows(values_only=True)
@@ -36,7 +37,7 @@ for file in glob("data/disreg_ecv23/*.xlsx"):
                     break
 
                 if f1 and f2:
-                    code_map[f1] = f2
+                    code_map[f1] = f"{wb_name}_{f2}"
         elif sheet.title.startswith("Tablas"):
             map = None
             in_key = None
@@ -44,7 +45,7 @@ for file in glob("data/disreg_ecv23/*.xlsx"):
             for row in rows:
                 f1, f2 = row[:2]
                 if not in_key and isinstance(f1, str):
-                    map = f1
+                    map = f"{wb_name}_{f1}"
                     in_key = True
                     next(rows)
                 elif in_key:
@@ -53,17 +54,16 @@ for file in glob("data/disreg_ecv23/*.xlsx"):
                     else:
                         in_key = False
 
-    # Write CSVs
-    for code, map in code_map.items():
-        path = f"{CODE_DIR}/{code}.csv"
-        if os.path.exists(path):
-            raise Exception(f"ERROR: code already exists: {path}")
-        with open(path, "w") as codefile:
-            code_writer = csv.writer(codefile)
-            code_writer.writerow(["id", "desc"])
-            for key, val in maps[map]:
-                code_writer.writerow([key, val])
-
+# Write CSVs
+for map_name, map in maps.items():
+    path = f"{CODE_DIR}/{map_name}.csv"
+    if os.path.exists(path):
+        raise Exception(f"ERROR: code already exists: {path}")
+    with open(path, "w") as codefile:
+        code_writer = csv.writer(codefile)
+        code_writer.writerow(["id", "desc"])
+        for key, val in map:
+            code_writer.writerow([key, val])
 
 # Build the database
 try:
@@ -77,3 +77,7 @@ for csv_path in glob("data/**/*.csv", recursive=True):
     for path in glob(csv_path):
         tbl = os.path.splitext(os.path.basename(path))[0]
         con.execute(f"CREATE TABLE {tbl} AS SELECT * FROM read_csv('{path}')")
+
+# create views (translates from codes to maps)
+for code, map in code_map.items():
+    con.execute(f"CREATE VIEW {code} AS SELECT * FROM {map};")
